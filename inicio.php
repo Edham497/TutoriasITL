d8f35c49651b3fe309000c7b9474f3aa31aca096
Edham <jorge.lrs497@gmail.com>
2018-12-02 16:45:03 -0600

    implementando la tabla de prueba
    implementando la tabla de prueba y correcciones css

--- inicio.php
+++ inicio.php
@@ -1,16 +1,19 @@
 <html>
     <head></head>
-    <body class="fRoboto">
+    <body class="fRoboto pdgT50">
         <header></header>
-        <main class="blueGrad setMaxSize col sc pdg10">
+        <main class="blueGrad setMaxSize col sc">
             <div class="nota platino1">Bienvenido al Sistema de Tutorias ITL</div>
+            <div class="pdg10"></div>
+            <?php include 'assets/tablaresponsiva'?>
         </main>
     </body>
     <footer class="platino5 pdg25"></footer>
     <script src="js/edhamJS.js" onload="main()">
     </script>
     <script type="text/javascript">
-    	importar('assets/header', 'header')
+    	importar('assets/header', 'header');
     </script>
-    <script type="text/javascript" src="js/transiciones.js" onload="main()"></script>
+    <script type="text/javascript" src="js/transiciones.js" onload="main()">
+    </script>
 </html>
